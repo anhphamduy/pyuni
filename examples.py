338f8da5cbeee89be48e9dfae8fafd796a28de8b
anhpham <anhphamduy@outlook.com>
2020-04-10 23:39:24 +1000

    Use context manager for scraper

--- examples.py
+++ examples.py
@@ -5,6 +5,6 @@ from pyuni import Client, University, Scraper
 university_scraper = University.UNIVERSITY_OF_MELBOURNE
 
 with Client(headless=False) as client:
-    scraper = Scraper(client=client, university_scraper=university_scraper)
-    scraper.authenticate(os.environ['TEST_USERNAME'], os.environ['TEST_PASSWORD'])
-    scraper.scrape()
+    with Scraper(client=client, university_scraper=university_scraper) as scraper:
+        scraper.authenticate(os.environ['TEST_USERNAME'], os.environ['TEST_PASSWORD'])
+        scraper.scrape()
